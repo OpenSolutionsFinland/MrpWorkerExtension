6cf2de702b15b088e71168c1631a840777b28719
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-09-30 09:09:18 +0300

    init file and module information

---
+++ __openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) Open Solutions Finland 2013.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name" : "Manufacturing Worker Extension",
+    "version" : "1.0",
+    "author" : "Open Solutions Finland",
+    "description" : """
+*** Manufacturing Worker Extension ***
+Simplifies 
+***********************************
+    """,
+    "website" : "http://www.opensolutions.fi",
+    "depends" : ["base","mrp","stock"],
+    "category" : "Manufacturing",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data" : [
+              'mrp_extension_view.xml'
+                    ],
+    'test': [
+             ],
+    'installable': True,
+    'active': False,
+    'certificate': '',
+}
