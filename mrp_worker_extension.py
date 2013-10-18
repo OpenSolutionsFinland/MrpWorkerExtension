2775e8ed26c5a03bfac26a72b7517645944fbe17
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-10-18 14:59:28 +0300

    added produced quantity to mrp.product.produce

--- mrp_worker_extension.py
+++ mrp_worker_extension.py
@@ -4,47 +4,47 @@ import openerp.addons.decimal_precision as dp
 class mrp_worker_extension(osv.osv_memory):
 
     _name='mrp.product.produce'
-	_inherit='mrp.product.produce'
-	
-	def _get_product_qty(self, cr, uid, context=None):
+    _inherit='mrp.product.produce'
+    
+    def _get_product_qty(self, cr, uid, context=None):
 
-		if context is None:
-			context = {}
-		prod = self.pool.get('mrp.production').browse(cr, uid, context['active_id'], context=context)
-		done = 0.0
-		for move in prod.move_created_ids2:
-			if move.product_id == prod.product_id:
-				if not move.scrapped:
-					done += move.product_qty
-		if prod.product_uos_qty > 0.0:
-			return prod.product_uos_qty
+        if context is None:
+            context = {}
+        prod = self.pool.get('mrp.production').browse(cr, uid, context['active_id'], context=context)
+        done = 0.0
+        for move in prod.move_created_ids2:
+            if move.product_id == prod.product_id:
+                if not move.scrapped:
+                    done += move.product_qty
+        if prod.product_uos_qty > 0.0:
+            return prod.product_uos_qty
 
-		return (prod.product_qty - done) or prod.product_qty
+        return (prod.product_qty - done) or prod.product_qty
 
-	def do_produce(self, cr, uid, ids, context=None):
-		print 'mrp_worker_extension.do_produce()'
-		super(mrp_worker_extension, self).do_produce(cr, uid, ids, context=context)
-		# get produced product's latest stock move and display a form
-		prod = self.pool.get('mrp.production').browse(cr, uid,context['active_id'], context=context)
-		print 'latest move: ' + str(prod.move_created_ids2)
-		cr.execute("select * from ir_model_data where name = 'mrp_extension_view'")
-		resource_id = cr.dictfetchall()[0]['res_id']
+    def do_produce(self, cr, uid, ids, context=None):
+        print 'mrp_worker_extension.do_produce()'
+        super(mrp_worker_extension, self).do_produce(cr, uid, ids, context=context)
+        # get produced product's latest stock move and display a form
+        prod = self.pool.get('mrp.production').browse(cr, uid,context['active_id'], context=context)
+        print 'latest move: ' + str(prod.move_created_ids2)
+        cr.execute("select * from ir_model_data where name = 'mrp_extension_view'")
+        resource_id = cr.dictfetchall()[0]['res_id']
 
-		return {
-			'view_id': 'mrp_worker_extension.mrp_extension_view',
-			"views": [(resource_id,'form')],
-			"type": "ir.actions.act_window",
-			'view_type': 'form',
-			'res_model': 'stock.move',
-			'res_id': prod.move_created_ids2[len(prod.move_created_ids2)-1].id,
-			'view_mode': 'form',
-			'target': 'new',
-			'context': context,
-		}
-	_defaults = {
-		'product_qty': _get_product_qty,
-		#'produced_qty': _get_produced_qty
-	}
+        return {
+            'view_id': 'mrp_worker_extension.mrp_extension_view',
+            "views": [(resource_id,'form')],
+            "type": "ir.actions.act_window",
+            'view_type': 'form',
+            'res_model': 'stock.move',
+            'res_id': prod.move_created_ids2[len(prod.move_created_ids2)-1].id,
+            'view_mode': 'form',
+            'target': 'new',
+            'context': context,
+        }
+    _defaults = {
+        'product_qty': _get_product_qty,
+        #'produced_qty': _get_produced_qty
+    }
 
 mrp_worker_extension()
 
