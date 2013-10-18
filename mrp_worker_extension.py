48465df1b678c7b54ba44c65894d47887ccacb11
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-10-18 15:36:43 +0300

    added produced quantity to mrp.product.produce

--- mrp_worker_extension.py
+++ mrp_worker_extension.py
@@ -11,10 +11,15 @@ class mrp_worker_extension(osv.osv_memory):
     }
     
     def _get_produced_qty(self, cr, uid, context=None):
+        print '_get_produced_qty()'
+        prod = None
         if context is None:
             print 'context none'
             context = {}
-        prod = self.pool.get('mrp.production').browse(cr, uid, [], context=context)
+            prod = self.pool.get('mrp.production').browse(cr, uid, [], context=context)
+        else:
+            prod = self.pool.get('mrp.production').browse(cr, uid, context['active_id'], context=context)
+            
         done = 0.0
         for move in prod.move_created_ids:
             if move.product_id == prod.product_id:
@@ -24,7 +29,7 @@ class mrp_worker_extension(osv.osv_memory):
         return (prod.product_qty - done) or prod.product_qty
         
     def _get_product_qty(self, cr, uid, context=None):
-
+        
         if context is None:
             context = {}
         prod = self.pool.get('mrp.production').browse(cr, uid, context['active_id'], context=context)
